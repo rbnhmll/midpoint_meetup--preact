e767ee11d3b9afa28999ed437b15234a05303e4a
Robin Hamill <hamillrobin@gmail.com>
2017-12-13 14:10:25 -0500

    Add functionality to get user city location based on IP

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import { h, Component } from 'preact';
 
 import scrollToElement from 'scroll-to-element';
 import 'mapbox.js';
+import Axios from 'axios';
 import 'font-awesome/css/font-awesome.min.css';
 
 import Banner from './banner';
@@ -18,28 +19,68 @@ class App extends Component {
 		this.state = {
 			results: [],
 			show_modal: false,
-			map: ''
+			map: '',
+			mapBoxKey: 'pk.eyJ1IjoicmJuaG1sbCIsImEiOiI3NjY4ZDk5NjFhMTYyMDMxMWFmMmM5YWEzMzlkMDgwZiJ9.Ep7u1zX_6SFI94jPki9O-w'
 		};
 		this.setResults = this.setResults.bind(this);
 		this.toggleModal = this.toggleModal.bind(this);
 		this.closeModal = this.closeModal.bind(this);
 		this.displayVenues = this.displayVenues.bind(this);
+		this.defineIPLocation = this.defineIPLocation.bind(this);
+		this.setMap = this.setMap.bind(this);
 	}
 
-	componentDidMount() {
-		// Initialize mapbox
-		const map = L.map('map').setView([43.65323, -79.38318], 12);
+	async componentDidMount() {
+		const result = await this.defineIPLocation();
+		if (result !== "error") {
+			const cityLatLng = await this.getCityLatLng(result.data.city);
+			this.setMap(cityLatLng);
+		} else {
+			this.setMap(result);			
+		}
+	}
 
+	async defineIPLocation() {
+		try {
+			const data = await Axios.get(`https://freegeoip.net/json/`);
+			return data;
+		} catch(err) {
+			console.error(err, "Failed to load IP, likely due to VPN usage. This is blocked by https://freegeoip.net/")
+			return "error";
+		}
+	}
+
+	async getCityLatLng(city) {
+		try {
+			const cityLatLng = await Axios.get(
+				`https://api.mapbox.com/v4/geocode/mapbox.places/${city}.json?access_token=${this.state.mapBoxKey}`);
+			return cityLatLng;
+		} catch(err) {
+			console.error(err)
+		}
+	}
+	
+	setMap(locData) {
+		// Initialize mapbox
+		let map;
+		// Check if the ip
+		if (locData != "error") {
+			const cityCenter = locData.data.features[0].center;
+			map = L.map('map').setView([cityCenter[1], cityCenter[0]], 12);			
+		} else {
+			map = L.map('map').setView([43.65323, -79.38318], 12);
+		}
+	
 		// Disable scrolling when hovering on map
 		map.scrollWheelZoom.disable();
-
+	
 		// Some Mapbox specifics for on load [suplied by mapbox]
 		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 			maxZoom: 18,
 			id: 'rbnhmll.n1oca4ci',
 			accessToken: 'pk.eyJ1IjoicmJuaG1sbCIsImEiOiI3NjY4ZDk5NjFhMTYyMDMxMWFmMmM5YWEzMzlkMDgwZiJ9.Ep7u1zX_6SFI94jPki9O-w'
 		}).addTo(map);
-
+	
 		this.setState({ map });
 	}
 
